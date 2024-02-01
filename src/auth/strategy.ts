e44321877687f43d97a80089cf775ec20d0674e0
Andres620 <andres.1059814779@gmail.com>
2024-02-01 16:10:22 -0500

    Integración de las dos estrategias (ms-logica-negocio y ms-seguridad)

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -9,6 +9,8 @@ import {inject} from '@loopback/context';
 import {HttpErrors, Request} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
 import parseBearerToken from 'parse-bearer-token';
+import {ConfiguracionSeguridad} from '../config/configuracion.seguridad';
+const fetch = require('node-fetch');
 
 export class AuthStrategy implements AuthenticationStrategy {
   name: string = 'auth';
@@ -30,15 +32,44 @@ export class AuthStrategy implements AuthenticationStrategy {
       let accion = this.metadata[0].options![1];
       console.log('Metadata', this.metadata);
 
-      let continuar: boolean = false;
+      // Conectar con ms de seguridad
+      const datos = {
+        token: token,
+        idMenu: idMenu,
+        accion: accion,
+      };
 
-      if (continuar) {
-        let perfil: UserProfile = Object.assign({
-          permitido: 'OK',
-        });
-        return perfil;
-      } else {
-        return undefined;
+      const urlValidarPermisos = `${ConfiguracionSeguridad.enlaceMicroservicioSeguridad}/validar-permisos`;
+      let res = undefined;
+      try {
+        await fetch(urlValidarPermisos, {
+          method: 'post',
+          body: JSON.stringify(datos),
+          headers: {
+            'Content-Type': 'application/json',
+            //Authorization: `Bearer ${token}`,  <-- Tambien puedo enviar el token desde el header por si no queremos enviarlo desde la peticion
+          },
+        })
+          .then((res: any) => res.json())
+          .then((json: any) => {
+            console.log('Respuesta: ');
+            console.log(json);
+            console.log('Conectado con ms de seguridad');
+            res = json;
+          });
+
+        if (res) {
+          let perfil: UserProfile = Object.assign({
+            permitido: 'OK',
+          });
+          return perfil;
+        } else {
+          return undefined;
+        }
+      } catch (e) {
+        throw new HttpErrors[401](
+          'No se tienen permisos sobre la acción solicitada',
+        );
       }
     }
     console.log('Ejecutando estrategia desded lógica de negocio');
